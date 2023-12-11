f832776a778723a7cfa5ccc57da161ff06f6a933
One <43485962+c-git@users.noreply.github.com>
2023-12-11 15:52:13 -0500

    Starter code for day1

---
+++ src/day01.rs
@@ -0,0 +1,19 @@
+use actix_web::{web, Scope};
+use tracing::info;
+
+pub(crate) fn scope() -> Scope {
+    web::scope("/1").route("/{num1}/{num2}", web::get().to(task1_cube_the_bits))
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Nums {
+    num1: i32,
+    num2: i32,
+}
+
+#[tracing::instrument]
+async fn task1_cube_the_bits(nums: web::Path<Nums>) -> actix_web::Result<String> {
+    info!("Start");
+    info!("{} {}", nums.num1, nums.num2);
+    todo!("yay")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use actix_web::web::{self, ServiceConfig};
 
+mod day01;
 mod day_minus_1;
 
 pub fn modify_service_config(cfg: &mut ServiceConfig) {
     cfg.route("/", web::get().to(day_minus_1::task1_everything_is_ok));
     cfg.service(day_minus_1::scope());
+    cfg.service(day01::scope());
 }
