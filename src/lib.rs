e0617efdccf0151fd0528d7b801bca3cc364f714
One <43485962+c-git@users.noreply.github.com>
2023-12-11 17:25:47 -0500

    Add 404 page
    Default page had no body so without headers you couldn't tell

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use actix_web::web::{self, ServiceConfig};
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse,
+};
+use tracing::error;
 
 mod day01;
 mod day_minus_1;
@@ -7,4 +11,11 @@ pub fn modify_service_config(cfg: &mut ServiceConfig) {
     cfg.route("/", web::get().to(day_minus_1::task1_everything_is_ok));
     cfg.service(day_minus_1::scope());
     cfg.service(day01::scope());
+    cfg.default_service(web::route().to(not_found));
+}
+
+#[tracing::instrument(name = "Not Found", skip())]
+pub async fn not_found() -> actix_web::Result<HttpResponse> {
+    error!("Failed to match route");
+    Ok(HttpResponse::NotFound().body("404 Not found\n"))
 }
