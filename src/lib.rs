3e0d49c044a83a4b5373454ab8ce3e3430203f3a
One <43485962+c-git@users.noreply.github.com>
2023-12-11 20:12:24 -0500

    Add handler to cath 404 to show the route

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,14 @@ pub fn modify_service_config(cfg: &mut ServiceConfig) {
     cfg.route("/", web::get().to(day_minus_1::task1_everything_is_ok));
     cfg.service(day_minus_1::scope());
     cfg.service(day01::scope());
+    cfg.route("/{path:.*}", web::get().to(catch_unmatched));
     cfg.default_service(web::route().to(not_found));
+}
+
+#[tracing::instrument]
+async fn catch_unmatched(path: web::Path<String>) -> actix_web::Result<HttpResponse> {
+    error!("Caught an unmatched route");
+    Ok(HttpResponse::NotFound().body("404 Not found (Logged)\n"))
 }
 
 #[tracing::instrument]
