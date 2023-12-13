a0455a89d4e6c0f797c8bcacc2f0e437ee9f6e99
One <43485962+c-git@users.noreply.github.com>
2023-12-13 15:27:42 -0500

    Day 11 completed

--- src/day11.rs
+++ src/day11.rs
@@ -1,6 +1,8 @@
 use actix_files::NamedFile;
 use actix_multipart::form::{tempfile::TempFile, MultipartForm};
 use actix_web::{error::ErrorBadRequest, web, Error, HttpRequest, HttpResponse, Responder, Scope};
+use image::Rgb;
+use tracing::info;
 
 pub(crate) fn scope() -> Scope {
     web::scope("/11")
@@ -33,10 +35,18 @@ async fn task2_bull_mode_activated(
         .decode()
         .map_err(ErrorBadRequest)?;
     let pixels = match img.as_rgb8() {
-        Some(value) => value,
+        Some(value) => value.pixels(),
         None => return Err(ErrorBadRequest("Unable to access image pixels")),
     };
-    dbg!(pixels[(0, 0)]);
-
-    Ok(HttpResponse::Ok())
+    let result: usize = pixels
+        .map(|Rgb([red, green, blue])| {
+            if *red as usize > *blue as usize + *green as usize {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+    info!("magical red count is: {result}");
+    Ok(HttpResponse::Ok().body(result.to_string()))
 }
