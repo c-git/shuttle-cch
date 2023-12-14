2f73cad3cc84ca4d95c11020fe8860695c8318b3
One <43485962+c-git@users.noreply.github.com>
2023-12-13 20:23:38 -0500

    First attempt
    Seems to create map for each thread

--- src/day12.rs
+++ src/day12.rs
@@ -1,20 +1,35 @@
-use actix_web::{web, Scope};
+use std::collections::HashMap;
+
+use actix_web::{web, HttpResponse, Scope};
+use tokio::{sync::Mutex, time::Instant};
 use tracing::info;
 
+type Map = HashMap<String, Instant>;
+type WrappedMap = Mutex<Map>;
+
 pub(crate) fn scope() -> Scope {
+    let map = web::Data::new(WrappedMap::new(Map::new()));
     web::scope("/12")
+        .app_data(map)
         .route("/save/{id}", web::post().to(task1_save))
         .route("/load/{id}", web::get().to(task1_load))
 }
 
 #[tracing::instrument]
-async fn task1_save() -> String {
-    info!("Task 1 save");
-    todo!()
+async fn task1_save(id: web::Path<String>, map: web::Data<WrappedMap>) -> HttpResponse {
+    let mut map = map.lock().await;
+    let id = id.into_inner();
+    info!("Saving ID: {id:?}");
+    map.insert(id, Instant::now());
+    HttpResponse::Ok().finish()
 }
 
 #[tracing::instrument]
-async fn task1_load() -> String {
-    info!("Task 1 load");
-    todo!()
+async fn task1_load(
+    id: web::Path<String>,
+    map: web::Data<WrappedMap>,
+) -> actix_web::Result<HttpResponse> {
+    let result = 4;
+    info!("Result = {result}");
+    Ok(HttpResponse::Ok().body(result.to_string()))
 }
