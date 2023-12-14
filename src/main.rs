d6548ab445b72292718a0ceabb0fd8bd5ff2f08c
One <43485962+c-git@users.noreply.github.com>
2023-12-13 21:34:28 -0500

    Remove dead code left in error

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
-
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Day -1 Task 1"
-}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
