dd22b9d49ca350d218d7e55061a9e2d7bcc9c976
One <43485962+c-git@users.noreply.github.com>
2023-12-15 16:07:37 -0500

    Add logging middleware

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{
+    middleware::Logger,
     web::{self, ServiceConfig},
     HttpRequest, HttpResponse,
 };
@@ -11,11 +12,11 @@ mod day_minus_1;
 
 pub fn modify_service_config(cfg: &mut ServiceConfig) {
     cfg.route("/", web::get().to(day_minus_1::task1_everything_is_ok));
-    cfg.service(day_minus_1::scope());
-    cfg.service(day01::scope());
-    cfg.service(day11::scope());
-    cfg.service(day12::scope());
-    cfg.default_service(web::route().to(not_found));
+    cfg.service(day_minus_1::scope().wrap(Logger::default()));
+    cfg.service(day01::scope().wrap(Logger::default()));
+    cfg.service(day11::scope().wrap(Logger::default()));
+    cfg.service(day12::scope().wrap(Logger::default()));
+    cfg.default_service(web::route().to(not_found).wrap(Logger::default()));
 }
 
 #[tracing::instrument]
