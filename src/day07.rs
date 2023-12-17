9685c99b85bd89fbd2985cda16a7fa167a062d4c
One <43485962+c-git@users.noreply.github.com>
2023-12-17 15:12:59 -0500

    Day 7 Task 1

--- src/day07.rs
+++ src/day07.rs
@@ -1,15 +1,24 @@
-use actix_web::{web, HttpRequest, HttpResponse, Scope};
+use actix_web::{error, web, HttpRequest, HttpResponse, Scope};
+use base64::{engine::general_purpose, Engine};
 use tracing::info;
 pub(crate) fn scope() -> Scope {
     web::scope("/7").route("/decode", web::get().to(task1_based_encoding_64_th_edition))
 }
 
 #[tracing::instrument]
-async fn task1_based_encoding_64_th_edition(req: HttpRequest) -> HttpResponse {
+async fn task1_based_encoding_64_th_edition(req: HttpRequest) -> actix_web::Result<HttpResponse> {
     let cookie = match req.cookie("recipe") {
         Some(val) => val,
-        None => return HttpResponse::BadRequest().body("recipe cookie not found"),
+        None => return Err(error::ErrorBadRequest("recipe cookie not found")),
     };
     info!("Incoming Cookie: {cookie}");
-    HttpResponse::NotImplemented().finish()
+
+    let value = cookie.value();
+    let decoded = general_purpose::STANDARD
+        .decode(value)
+        .map_err(error::ErrorBadRequest)?;
+    let json = String::from_utf8(decoded).map_err(error::ErrorBadRequest)?;
+    info!(json);
+
+    Ok(HttpResponse::Ok().body(json))
 }
