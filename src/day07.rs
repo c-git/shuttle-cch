82e9ccd9abbbbe1a4c655f90b131adf6ec7c8a79
One <43485962+c-git@users.noreply.github.com>
2023-12-17 20:51:41 -0500

    Day 7 Task 2

--- src/day07.rs
+++ src/day07.rs
@@ -1,8 +1,11 @@
 use actix_web::{error, web, HttpRequest, HttpResponse, Scope};
 use base64::{engine::general_purpose, Engine};
+use serde::{Deserialize, Serialize};
 use tracing::info;
 pub(crate) fn scope() -> Scope {
-    web::scope("/7").route("/decode", web::get().to(task1_based_encoding_64_th_edition))
+    web::scope("/7")
+        .route("/decode", web::get().to(task1_based_encoding_64_th_edition))
+        .route("/bake", web::get().to(task2_the_secret_cookie_recipe))
 }
 
 #[tracing::instrument]
@@ -28,7 +31,59 @@ fn extract_recipe(req: HttpRequest) -> actix_web::Result<String> {
     Ok(json)
 }
 
+#[derive(Debug, Deserialize)]
+struct Input {
+    recipe: Ingredients,
+    pantry: Ingredients,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Ingredients {
+    flour: usize,
+    sugar: usize,
+    butter: usize,
+    #[serde(rename = "baking powder")]
+    baking_powder: usize,
+    #[serde(rename = "chocolate chips")]
+    chocolate_chips: usize,
+}
+
+#[derive(Debug, Serialize)]
+struct Output {
+    cookies: usize,
+    pantry: Ingredients,
+}
+
 #[tracing::instrument]
 async fn task2_the_secret_cookie_recipe(req: HttpRequest) -> actix_web::Result<HttpResponse> {
-    todo!()
+    let input: Input =
+        serde_json::from_str(&extract_recipe(req)?).map_err(error::ErrorBadRequest)?;
+    info!("Parsed Input = {input:?}");
+
+    let mut cookies = usize::MAX;
+    if input.recipe.flour > 0 {
+        cookies = cookies.min(input.pantry.flour / input.recipe.flour);
+    }
+    if input.recipe.sugar > 0 {
+        cookies = cookies.min(input.pantry.sugar / input.recipe.sugar);
+    }
+    if input.recipe.butter > 0 {
+        cookies = cookies.min(input.pantry.butter / input.recipe.butter);
+    }
+    if input.recipe.baking_powder > 0 {
+        cookies = cookies.min(input.pantry.baking_powder / input.recipe.baking_powder);
+    }
+    if input.recipe.chocolate_chips > 0 {
+        cookies = cookies.min(input.pantry.chocolate_chips / input.recipe.chocolate_chips);
+    }
+    let mut pantry = input.pantry.clone();
+    pantry.flour -= input.recipe.flour * cookies;
+    pantry.sugar -= input.recipe.sugar * cookies;
+    pantry.butter -= input.recipe.butter * cookies;
+    pantry.baking_powder -= input.recipe.baking_powder * cookies;
+    pantry.chocolate_chips -= input.recipe.chocolate_chips * cookies;
+    let result = Output { cookies, pantry };
+    info!("result = {result:?}");
+
+    Ok(HttpResponse::Ok().json(result))
 }
